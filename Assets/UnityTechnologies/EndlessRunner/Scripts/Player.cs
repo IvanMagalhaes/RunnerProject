dec23ba9896879e8cac28fda52866a2b646ede7e
IvanMagalhaes <ivanmds01@gmail.com>
2024-11-06 14:00:14 -0300

    Pulo
    criação do pulo do personagem

--- Assets/UnityTechnologies/EndlessRunner/Scripts/Player.cs
+++ Assets/UnityTechnologies/EndlessRunner/Scripts/Player.cs
@@ -8,21 +8,38 @@ public class Player : MonoBehaviour
     public int velocidade;
     public Animator animacoesPlayer;
     public Transform ObjAuxiliar;
+    public LayerMask LayerPlataforma;
+    public bool NoChao;
+    public int ForcaPulo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        NoChao = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space")){
+            Pulo();
+        }
     }
 
     void FixedUpdate(){
         RBPlayer.velocity = new Vector2(velocidade, RBPlayer.velocity.y);
-        if(Physics2D.OverlapCircle(ObjAuxiliar.position, 0.2f, ));
+        if(Physics2D.OverlapCircle(ObjAuxiliar.position, 0.2f, LayerPlataforma)){
+            NoChao = true;
+            animacoesPlayer.SetBool("NoChao", true);
+        }else{
+            NoChao = false;
+            animacoesPlayer.SetBool("NoChao", false);
+        }
+    }
+
+    void Pulo(){
+        if(NoChao){
+        RBPlayer.AddForce(new Vector2 (0,ForcaPulo));
+        }
     }
 }
